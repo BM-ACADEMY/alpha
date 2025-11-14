0a1d280f4abd38ded4c3b1305eb54bb55dc98bd2
BM-ACADEMY <teamkamarweb@gmail.com>
2025-11-14 12:23:31 +0530

    profit percentage completed

--- server/controller/planController.js
+++ server/controller/planController.js
@@ -55,3 +55,11 @@ exports.deletePlan = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+exports.getPlans = async (req, res) => {
+  try {
+    const plans = await Plan.find();
+    res.status(200).json(plans);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
